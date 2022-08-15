7f577e60759fe916c16d2649f7008ebc53147a9e
jiyoonjang <rchel02@naver.com>
2022-08-15 21:11:30 +0900

    1차 완료

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,51 +1,8 @@
 $(function(){
     
-    // 메뉴
-    $('.header .btn-gnb-mb').click(function(e){
-        e.preventDefault();
-        if ($('.header .gnb-area-mb').hasClass('active')) {
-            $('.header .gnb-area-mb').removeClass('active')
-        } else {
-            $('.header .gnb-area-mb').addClass('active')
-        }
-        
-        $('body').toggleClass('hidden');
-    })
+   
 
-    $('.gnb-area-mb nav a').click(function(e){
-        e.preventDefault();
-        if($('.gnb-area-mb').hasClass('active')){
-            $('.gnb-area-mb').removeClass('active');
-            $('body').removeClass('hidden');
-        }
-    })
-
-    $('.scrollY').each(function (index, element) {
-
-        //   hearthealth =gsap.timeline({
     
-        // })
-        // fromVal = $(this).data('from')
-        // toVal = $(this).data('to')
-        // console.log(fromVal + '///' + toVal );
-    
-    
-    
-        gsap.fromTo(element, {
-          y: 200,
-          opacity:0,
-        }, {
-          scrollTrigger: {
-            trigger: element,
-            start: "0% 70%", //트리거기준 0%위치, 윈도우기준0%
-            end: "70% 70%",
-            // markers:true,
-            scrub: 1,
-          },
-          y: 0,
-          opacity:1,
-        })
-      })
 
 
 
@@ -53,20 +10,6 @@ $(function(){
         // large
         "(min-width: 1025px)": function() {
            
-        
-             
-            gsap.to('.sc-about .desc-area',{
-                scrollTrigger:{
-                    trigger:'.sc-about',
-                    start:"30% 50%",//트리거기준 0%위치, 윈도우기준0%
-                    end:"40% 50%",
-                    scrub:1,
-                    markers:true,
-                },
-                y:-50,
-                opacity:1,
-            })
-            
         },
         // medium
         "(min-width: 768px) and (max-width: 1024px)": function() {
@@ -78,8 +21,23 @@ $(function(){
         },
         // all
         "all": function() {
+          
 
-            
+          $('.scrollY').each(function (index, element) {
+            gsap.fromTo(element, {
+              y: 200,
+              opacity:0,
+            }, {
+              scrollTrigger: {
+                trigger: element,
+                start: "0% 70%", //트리거기준 0%위치, 윈도우기준0%
+                end: "70% 70%",
+                scrub: 1,
+              },
+              y: 0,
+              opacity:1,
+            })
+          })
 
             
         }
